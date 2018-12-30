04a0c2ec0177a8a7fdf1095ea17c84be84624c94
@markurquhart <markurquhart@Marks-MacBook-Pro.local>
2018-12-30 10:12:08 -0500

    Adding weight module
    Added in weight object and appropriate transforms from kgs to lbs

--- transforms.js
+++ transforms.js
@@ -1,21 +1,21 @@
 <script type="text/javascript">
-	
+
 	function distance () {
-		
-// 		Define the meters object first, put in a hidden span ID	
-		var meters = document.getElementById("distance_hidden").innerHTML;		
+
+// 		Define the meters object first, put in a hidden span ID
+		var meters = document.getElementById("distance_hidden").innerHTML;
 // 		Define the miles by doing basic math
 		var miles = meters*0.000621371192;
 // 		Restrict the returned value to two decimal places
 			miles = Number.parseFloat(miles).toFixed(2);
-// 		Place the new clean miles value to visible span ID					
+// 		Place the new clean miles value to visible span ID
 		document.getElementById('distance').innerHTML = miles + "&nbsp;" + "mi";
-	
+
 	};
-	
+
 
 	function time () {
-		
+
 // 		Define the seconds object first, put in a hidden span ID
 		var seconds = document.getElementById("time_hidden").innerHTML;
 // 		Define the hours by doing basic math
@@ -24,25 +24,24 @@
 			hours = Number.parseFloat(hours).toFixed(2);
 // 		Place the new clean hours value to visible span ID
 		document.getElementById('time').innerHTML = hours + "&nbsp;" + "hrs";
-		
+
 	};
-	
+
 	function distancerun () {
-		
-// 		Define the meters object first, put in a hidden span ID	
-		var metersrun = document.getElementById("distancerun_hidden").innerHTML;		
+
+// 		Define the meters object first, put in a hidden span ID
+		var metersrun = document.getElementById("distancerun_hidden").innerHTML;
 // 		Define the miles by doing basic math
 		var milesrun = metersrun*0.000621371192;
 // 		Restrict the returned value to two decimal places
 			milesrun = Number.parseFloat(milesrun).toFixed(2);
-			console.log(milesrun);
-// 		Place the new clean miles value to visible span ID					
+// 		Place the new clean miles value to visible span ID
 		document.getElementById('distancerun').innerHTML = milesrun + "&nbsp;" + "mi";
-		
+
 	};
-	
+
 	function timerun () {
-		
+
 // 		Define the seconds object first, put in a hidden span ID
 		var secondsrun = document.getElementById("timerun_hidden").innerHTML;
 // 		Define the hours by doing basic math
@@ -51,12 +50,33 @@
 			hoursrun = Number.parseFloat(hoursrun).toFixed(2);
 // 		Place the new clean hours value to visible span ID
 		document.getElementById('timerun').innerHTML = hoursrun + "&nbsp;" + "hrs";
-		
+
 	};
-	
+
+	function weight () {
+
+// 		Define the current weight object, put in a hidden span ID
+		var currentweight = document.getElementById("weighthidden").innerHTML
+// 		Convert kgs to lbs
+		var fixedweight = currentweight*2.205;
+// 		Restruct the returned value to two decimal places
+			shownweight = Number.parseFloat(fixedweight).toFixed(2);
+// 		Place the new clean weight value to visible span ID
+		document.getElementById('weight').innerHTML = shownweight + "&nbsp;" + "lbs";
+// 		Set variables for weight left to lose
+		var remainingweight = shownweight - 195
+// 		Set decimal float to two decimals
+		remainingweight = Number.parseFloat(remainingweight).toFixed(2);
+		console.log(remainingweight);
+// 		Place this value into weight remaining ID
+		document.getElementById('poundstogo').innerHTML = "Left to lose:" + "&nbsp;" + remainingweight + "&nbsp;" + "lbs";
+
+	};
+
 	distance ();
 	time ();
 	distancerun ();
 	timerun ();
-	
+	weight ();
+
 </script>
